6d058b21798e2e13d4b0b1606969063b2ab24954
Lucas S <lucas.sorribes@gmail.com>
2024-02-20 16:49:59 +0100

    Style Numbers word in Poem

--- frontend/src/components/PlotinusSkull.jsx
+++ frontend/src/components/PlotinusSkull.jsx
@@ -2,7 +2,7 @@ const PlotinusSkull = ({goToRandomNumber}) => {
   return (
     <p 
       className='poem'
-      onClick={() => goToRandomNumber({noMedian: true})}
+      onClick={() => goToRandomNumber({noMedian: false})}
     >
       Never stop sculpting your own statue:
       contemplate the immediate simplicity of the <span className="poem-numbers">Numbers</span>;
