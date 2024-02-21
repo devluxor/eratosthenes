789b6e5e88826185e83796e67b3764e0fa07210d
Lucas S <lucas.sorribes@gmail.com>
2024-02-21 17:45:11 +0100

    Fix number 2 bug on App.jsx

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,7 @@ const SERVER = import.meta.env.VITE_SERVER
 const BASE_URL = `https://${SERVER}/api/eratosthenes/`
 
 export const primesAndMedian = async (n) => {
-  if (n <= 2) return []
+  if (n <= 1) return []
 
   try {
     const result = await axios.get(`${BASE_URL}${n}`)
